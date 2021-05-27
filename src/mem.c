a3b59d0b08ece057d0cfa90e87a9aee839d13b93
Miles Simpson <miles.a.simpson@gmail.com>
2021-05-27 16:17:11 -0700

    Implement accounting functionality

--- src/mem.c
+++ src/mem.c
@@ -29,14 +29,61 @@ void __real_free(void *ptr);
 void *__wrap_malloc(size_t size)
 {
     void *ptr = NULL;
+    alloc_t *next = head;
+
+    /* Allocate accounting entry */
+    head = __real_malloc(sizeof(alloc_t));
+    if (head == NULL) {
+        goto malloc_fail;
+    }
+    /* Allocate requested memory */
+    ptr = __real_malloc(size);
+    if (ptr == NULL) {
+       __real_free(head);
+       goto malloc_fail;
+    }
+    /* Populate accounting information and link */
+    head->alloc_ptr = ptr;
+    head->ret_addr = __builtin_return_address(0);
+    head->alloc_size = size;
+    head->next = next;
 
     /* Return pointer to requested memory */
     return ptr;
+
+malloc_fail:
+    /* Failure to allocate, restore state */
+    head = next;
+    return NULL;
 }
 
 /* free implementation. Signature and use is identical to stdlib free */
 void __wrap_free(void *ptr)
 {
+    alloc_t *entry = head;
+    alloc_t *prev = NULL;
+
+    /* If NULL, no need to do anything */
+    if (ptr == NULL) {
+        return;
+    }
+
+    /* Find accounting entry for pointer, assert if it's not located */
+    while (entry && (entry->alloc_ptr != ptr)) {
+        prev = entry;
+        entry = entry->next;
+    }
+    assert(entry != NULL);
+
+    /* Unlink and deallocate memory */
+    if (prev != NULL) {
+        prev->next = entry->next;
+    } else {
+        head = entry->next;
+    }
+    __real_free(ptr);
+    __real_free(entry);
+    return;
 }
 
 /* Memory report function. Returns number of blocks allocated.
